24d991549788ad5310f3f6a9f956bc39e704b0b7
Iain MacDonald <IJMacD@gmail.com>
2021-06-09 21:25:56 +0800

    Add React gubbins

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ISO8601TestPage from './Pages/ISO8601TestPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ISO8601TestPage />
+    </div>
+  );
+}
+
+export default App;
